21b81c6ef2abc659ecfb0c2196fc8339ca77024b
aviraj0403 <aviraj0403@gmail.com>
2024-09-05 08:41:20 +0530

    "Refactor main.jsx: removed unnecessary imports, extracted router configuration to separate file, and simplified component tree"

---
+++ src/router.jsx
@@ -0,0 +1,28 @@
+import React, { Suspense } from 'react';
+import { Route, createBrowserRouter, createRoutesFromChildren } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import Dashboard from './components/dashboard/Dashboard.jsx';
+import DiningTable from './components/dashboard/dinningTable/DinningTable.jsx';
+import PageNotFound from './components/404Page/PageNotFound.jsx';
+import BarCode from './components/dashboard/table/BarCode.jsx';
+import AddItem from './components/addItem/addItem.jsx';
+import FallbackPage from './pages/FallBack.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromChildren(
+    <Route path='/' element={<Layout />} >
+      <Route index element={
+        <Suspense fallback={<FallbackPage />}>
+          <Dashboard />
+        </Suspense>
+      } />
+      <Route path='dinning' element={<DiningTable />} />
+      <Route path='dinning/table' element={<DiningTable />} />
+      <Route path="/add-item" element={<AddItem />} />
+      <Route path='dinning/table/:id' element={<BarCode />} />
+      <Route path='*' element={<PageNotFound />} />
+    </Route>
+  )
+);
+
+export default router;
